f8f2c6d2869a5f9483cb3041c0a10af552189edb
Bryan Sullivan <sull8000@github.com>
2023-08-04 10:11:01 -0700

    skip-secret-scanning:true

--- src/Web/Constants.cs
+++ src/Web/Constants.cs
@@ -8,7 +8,7 @@ public static class Constants
     public const string BASKET_ID = "BasketId";
     public const string STORAGE_TEST = "18gryvHXuSVGDBcdJ3+QhRypNi413Kri8oalcQPAAZ7UGMHjaTVpSq4R9fYqzCsmZDnvK6AaE8Ce+AStDHNkpQ=="; 
 
-    // DEPRECATED
+    // DEPRECATED 
     // var AWS_STORAGE_ACCT = "AKIAZBQE345LKPTEAHQD"; 
     // var AWS_STORAGE_ACCESS = "wt6lVzza0QFx/U33PU8DrkMbnKiu+bv9jheR0h/D";
 
